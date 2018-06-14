a4f3cdd83de44ed6843eec9fbcaadee3147cbb84
zhangbron <bronzhang1993@163.com>
2018-06-14 21:13:13 +0800

    ex6

--- zhangbron/stanford_ml_source/stanford_exercise/machine-learning-ex6/ex6/dataset3Params.m
+++ zhangbron/stanford_ml_source/stanford_exercise/machine-learning-ex6/ex6/dataset3Params.m
@@ -11,6 +11,9 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
+C_list = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_list = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -22,9 +25,18 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+####error = zeros(8,8)
 
-
-
+for i=1:8
+  for j=1:8
+    model = svmTrain(X,y,C_list(i),@(x1, x2) gaussianKernel(x1, x2, sigma_list(j)));
+    predictions = svmPredict(model,Xval);
+    error(i,j) = mean(double(predictions ~= yval));
+  endfor
+endfor
+[C_i,sigma_j]= find(error==min(min(error)));
+C = C_list(C_i);
+sigma = sigma_list(sigma_j);
 
 
 
